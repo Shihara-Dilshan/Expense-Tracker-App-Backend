357b1e83c1a22bb46164570ebf250b64c3e7798e
root <root@kali.noobmaster>
2020-06-16 18:37:59 -0400

    first commit

---
+++ src/main/java/com/noobstack/expense/ExpenseApplication.java
@@ -0,0 +1,16 @@
+package com.noobstack.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class ExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApplication.class, args);
+
+	}
+
+}
